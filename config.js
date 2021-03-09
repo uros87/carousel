461b6de95cdb22467012be1e720ae9fdb354bc6f
uros87 <uros_brkic@yahoo.com>
2021-03-09 16:59:01 +0100

    modified carousel

--- config.js
+++ config.js
@@ -1,13 +1,23 @@
-let config = {
-  title_1: 'Some tle 1',
-  paragraph_1: 'Some p1',
-  imageUrl_1: 'https://source.unsplash.com/random',
-  title_2: 'Some title 2',
-  paragraph_2: 'Some p2',
-  imageUrl_2: 'https://source.unsplash.com/random',
-  title_3: 'Some title 3',
-  paragraph_3: 'Some p3',
-  imageUrl_3: 'https://source.unsplash.com/random',
-  from: moment('2021-03-07'),
-  to: moment('2021-03-31'),
-};
+let config = [
+  {
+    title: 'Some title 1',
+    paragraph: 'Some p1',
+    imageUrl: 'https://source.unsplash.com/random',
+    from: moment('2021-03-01'),
+    to: moment('2021-03-31'),
+  },
+  {
+    title: 'Some title 2',
+    paragraph: 'S p2',
+    imageUrl: 'https://source.unsplash.com/random',
+    from: moment('2021-03-01'),
+    to: moment('2021-03-31'),
+  },
+  {
+    title: 'Some title 3',
+    paragraph: 'Some p3',
+    imageUrl: 'https://source.unsplash.com/random',
+    from: moment('2021-03-01'),
+    to: moment('2021-03-31'),
+  },
+];
--- script.js
+++ script.js
@@ -1,28 +1,38 @@
-(function ($) {
-  'use strict';
-  $('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    dots: true,
-    items: 1,
-    nav: true,
-  });
-})(jQuery);
+$(document).ready(function () {
+  (function carouselConfig() {
+    config.forEach((el) => {
+      if (el.from.isBefore(moment()) && el.to.isAfter(moment())) {
+        let itemDiv = $('<div/>', {
+          class: 'item',
+        }).appendTo('.owl-carousel');
 
-(function carouselConfig() {
-  if (!config.from.isBefore(moment()) || !config.to.isAfter(moment())) {
-    $('.owl-carousel').hide();
-  }
+        let firstItemDiv = $('<div/>').appendTo(itemDiv);
 
-  $('.img-1').css('background-image', `url(${config.imageUrl_1})`);
-  $(`.item__1 h3`).text(`${config.title_1}`);
-  $(`.item__1 p`).text(`${config.paragraph_1}`);
+        let h = document.createElement('h3');
+        h.innerHTML = `${el.title}`;
 
-  $('.img-2').css('background-image', `url(${config.imageUrl_2})`);
-  $(`.item__2 h3`).text(`${config.title_2}`);
-  $(`.item__2 p`).text(`${config.paragraph_2}`);
+        let p = document.createElement('p');
+        p.innerHTML = `${el.paragraph}`;
 
-  $('.img-3').css('background-image', `url(${config.imageUrl_3})`);
-  $(`.item__3 h3`).text(`${config.title_3}`);
-  $(`.item__3 p`).text(`${config.paragraph_3}`);
-})();
+        firstItemDiv.append(h, p);
+
+        $('<div/>', {
+          class: 'img',
+          css: {
+            backgroundImage: `url(${el.imageUrl})`,
+          },
+        }).appendTo(itemDiv);
+      }
+    });
+  })();
+
+  (function ($) {
+    $('.owl-carousel').owlCarousel({
+      loop: true,
+      margin: 10,
+      dots: true,
+      items: 1,
+      nav: true,
+    });
+  })(jQuery);
+});
