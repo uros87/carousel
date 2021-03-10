e242c58cc3f44fe82c2fad001a014c752ae91057
uros87 <uros_brkic@yahoo.com>
2021-03-10 10:49:49 +0100

    Modified carousel

--- script.js
+++ script.js
@@ -2,26 +2,9 @@ $(document).ready(function () {
   (function carouselConfig() {
     config.forEach((el) => {
       if (el.from.isBefore(moment()) && el.to.isAfter(moment())) {
-        let itemDiv = $('<div/>', {
-          class: 'item',
-        }).appendTo('.owl-carousel');
-
-        let firstItemDiv = $('<div/>').appendTo(itemDiv);
-
-        let h = document.createElement('h3');
-        h.innerHTML = `${el.title}`;
-
-        let p = document.createElement('p');
-        p.innerHTML = `${el.paragraph}`;
-
-        firstItemDiv.append(h, p);
-
-        $('<div/>', {
-          class: 'img',
-          css: {
-            backgroundImage: `url(${el.imageUrl})`,
-          },
-        }).appendTo(itemDiv);
+        $('.owl-carousel').append(
+          `<div class="item"><div><h3>${el.title}</h3><p>${el.paragraph}</p></div><div><img src="${el.imageUrl}" class="img"></img></div></div`
+        );
       }
     });
   })();
